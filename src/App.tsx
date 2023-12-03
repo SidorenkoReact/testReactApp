ceac11638fa42649749668ff830e004542887ce1
exess <exess@yandex.ru>
2023-12-03 15:09:42 +1000

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Приложение на React!
+          Приложение на React! Внес изменения...
         </a>
       </header>
     </div>
